66551440d7dd35a29df654e7aab034987a2f9a1b
Quieter <2316696377@qq.com>
2020-09-27 23:52:01 +0800

    init

---
+++ gateway-deepin/src/main/java/com/fy/gateway/deepin/GatewayDeepinApp.java
@@ -0,0 +1,12 @@
+package com.fy.gateway.deepin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayDeepinApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayDeepinApp.class, args);
+    }
+}
